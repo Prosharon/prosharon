3df7f12daac070e6e2fe4aed863b052ce2cd84db
Soumya Mahbub <soumyabd2004@gmail.com>
2024-08-15 19:33:42 +0600

    Firebase hosting setup

---
+++ src/app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDa9mzfAhSW4rAvf_aGVulxbRkQzt3t7bA",
+  authDomain: "prosharon-2024.firebaseapp.com",
+  projectId: "prosharon-2024",
+  storageBucket: "prosharon-2024.appspot.com",
+  messagingSenderId: "857447560818",
+  appId: "1:857447560818:web:3b57250250a6cc238534eb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
